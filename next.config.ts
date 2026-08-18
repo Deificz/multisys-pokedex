3a67d2735970efe5a66378d62d8561db8c456aeb
Aeron Brylle Lazo <aeronbrylle.lazo@gmail.com>
2026-08-18 12:46:06 +0800

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/multisys-pokedex",
   images: {
     unoptimized: true,
     remotePatterns: [
