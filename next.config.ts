0b4ad8ace0699eeca313ac5a30ae30fc10d03fa7
Aeron Brylle Lazo <aeronbrylle.lazo@gmail.com>
2026-08-18 12:57:50 +0800

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
+const isProd =
+  process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-   basePath: isProd
+  basePath: isProd
     ? "/multisys-pokedex"
     : "",
   images: {
